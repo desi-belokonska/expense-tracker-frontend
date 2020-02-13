0422bcbafd5afa6cf79294b963b35706bcc4d0a8
Desislava Belokonska <desislava.belokonska@code.berlin>
2020-02-13 12:41:45 +0100

    set up linting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import { firestorePlugin } from 'vuefire'
 
-Vue.config.productionTip = false;
+Vue.use(firestorePlugin)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
